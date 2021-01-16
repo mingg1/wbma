57857496691bed6a454f9218464c81973f7fcfad
mingg1 <comg2843@gmail.com>
2021-01-16 18:19:42 +0200

    add files in MyApp folder

---
+++ components/ListItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+
+const ListItem = (props) => {
+  return (
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.imageBox}>
+        <Image
+          style={styles.image}
+          source={{uri: props.singleMedia.thumbnails.w160}}
+        />
+      </View>
+      <View style={styles.textBox}>
+        <Text style={styles.listTitle}>{props.singleMedia.title}</Text>
+        <Text>{props.singleMedia.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    borderRadius: 16,
+  },
+  imageBox: {
+    flex: 2,
+    height: '100%',
+  },
+  listTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 15,
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: '#eee',
+    borderRadius: 16,
+  },
+  textBox: {
+    flex: 2,
+    padding: 10,
+  },
+});
+
+export default ListItem;
