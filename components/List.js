7c64c9010fa73811ebb4e03addda14541d3a3022
mingg1 <comg2843@gmail.com>
2021-01-17 21:58:19 +0200

    initial commit for branch http-c

--- components/List.js
+++ components/List.js
@@ -1,33 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {FlatList} from 'react-native';
+import {useLoadMedia} from '../hooks/ApiHooks';
 import ListItem from './ListItem';
 
-const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
-
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-
-  const loadMedia = async (limit = 3) => {
-    try {
-      const listResponse = await fetch(baseUrl + 'media?limit=' + limit);
-      const listJson = await listResponse.json();
-      const media = Promise.all(
-        listJson.map(async (item) => {
-          const fileResponse = await fetch(baseUrl + 'media/' + item.file_id);
-          const fileJson = fileResponse.json();
-          console.log('results', fileJson);
-          return fileJson;
-        })
-      );
-      setMediaArray(media);
-    } catch (error) {
-      console.error('loadMedia error', error);
-    }
-  };
-
-  useEffect(() => {
-    loadMedia(5);
-  }, []);
+  const mediaArray = useLoadMedia();
 
   return (
     <FlatList
